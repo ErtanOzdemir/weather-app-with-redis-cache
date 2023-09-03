b03d4ebc71fa39d5e7c231f5b8ae5fde5547f732
Ertan Özdemir <ertan_kubi@hotmail.com>
2023-09-03 14:59:50 +0300

    Use Celcius

--- backend/src/routes/weather.ts
+++ backend/src/routes/weather.ts
@@ -21,7 +21,7 @@ weatherRouter.get(
     }
 
     const weather = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.OPEN_WEATHER_API_KEY}`
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.OPEN_WEATHER_API_KEY}&units=metric`
     );
 
     redisClient.set(
